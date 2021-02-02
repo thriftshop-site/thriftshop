4f488e82a907a487cc3d4a350594568dbc25b619
uriah <goldcoders@protonmail.com>
2021-02-02 10:24:02 +0800

    added theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import 'app/helpers/to_hex.dart';
 import 'app/routes/app_pages.dart';
 
 void main() {
@@ -10,6 +11,14 @@ void main() {
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Hex("#00275B").toColor,
+        accentColor: Hex("#B69859").toColor,
+        primaryColor: Hex("#00275B").toColor,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        splashColor: Colors.pink[50],
+      ),
     ),
   );
 }
