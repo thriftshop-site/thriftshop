60e31b99cb44942a09981e38ee871a0f58645564
uriah <goldcoders@protonmail.com>
2021-02-05 06:12:32 +0800

    added initial binding and firebase emulator

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
-import 'dart:io' show Platform;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:thriftshop/app/themes/main_theme.dart';
-import 'package:thriftshop/app/themes/theme_service.dart';
+import 'app/modules/home/bindings/home_binding.dart';
+import 'app/themes/main_theme.dart';
+import 'app/themes/theme_service.dart';
 import 'app/routes/app_pages.dart';
 
 // ignore: non_constant_identifier_names
-bool USE_FIRESTORE_EMULATOR = true;
+bool USE_FIRESTORE_EMULATOR = false;
 void main() async {
   await GetStorage.init();
 
@@ -17,13 +17,14 @@ void main() async {
   await Firebase.initializeApp();
 
   if (USE_FIRESTORE_EMULATOR) {
-    String host = Platform.isAndroid ? '10.0.2.2:8080' : 'localhost:8080';
+    String host = GetPlatform.isAndroid ? '10.0.2.2:8080' : 'localhost:8080';
     FirebaseFirestore.instance.settings =
         Settings(host: host, sslEnabled: false, persistenceEnabled: false);
   }
 
   runApp(
     GetMaterialApp(
+      initialBinding: HomeBinding(),
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
